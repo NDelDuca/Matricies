48ce35b06e9b11e8e44f0fe99e750ad563cb022c
Nicholas Del Duca <ndelduca@stevens.edu>
2018-11-18 18:42:58 -0500

    Added time

--- transp.c
+++ transp.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
+
+
+
+
+double time_diff(struct timeval st, struct timeval ed){
+
+  double st_ms, ed_ms, diff;
+
+  st_ms = (double)st.tv_sec*1000000 + (double)st.tv_usec;
+  ed_ms = (double)ed.tv_sec*1000000 + (double)ed.tv_usec;
+
+  diff = (double)ed_ms - (double)st_ms;
+
+  return diff;
+}
 
 void main(int argc, char const *argv[]){
+
+  struct timeval start, end;
 
   int i;
   int j;
@@ -11,21 +29,53 @@ void main(int argc, char const *argv[]){
   int n = strtol(argv[1], &p, 10);
 
   double *A = malloc(n * sizeof(float));
+  double *At = malloc(n * sizeof(float));
 
   // build the matrix
   for(i = 0; i < n; i++){
     for(j = 0; j < n; j++){
-      A[i * n + j] = 1;
+      A[i * n + j] = rand();
     }
   }
 
-  // transpose the matrix
+
+  // Print the input matrix
+
+  printf("Input Matrix:\n");
+
   for(i = 0; i < n; i++){
     for(j = 0; j < n; j++){
-      A[i * n + j] = 2;
+      printf("%d\t", A[i * n + j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+
+
+
+  // transpose the matrix
+  gettimeofday(&start, NULL);
+  for(i = 0; i < n; i++){
+    for(j = 0; j < n; j++){
+      At[i * n + j] = A[j * n + i];
     }
   }
+  A = At;
+  gettimeofday(&end, NULL);
 
-  free(A);
+  // Print the output matrix
+
+  printf("Output Matrix:\n");
+
+  for(i = 0; i < n; i++){
+    for(j = 0; j < n; j++){
+      printf("%d\t", A[i * n + j]);
+    }
+    printf("\n");
+  }
+
+  free(At);
+
+  printf("Time: %.0lf", time_diff(start, end));
 
 }
